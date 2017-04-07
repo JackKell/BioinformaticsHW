c5901a98c0c98d296241e6e6eeb61112fa8ea9da
Jackkell <bronzeeagle1@hotmail.com>
2017-04-06 17:29:24 -0700

    Fixed the problem with having a cor between genes instead of a cor between drugs

--- hw1/hw1.R
+++ hw1/hw1.R
@@ -1,28 +1,82 @@
+# Read the data from a table and converts a column to the row names
+readTableWithRowNames = function(filename, nameCol = 1) {
+  # Reads the data
+  data = read.delim(filename);
+  # Set rownames
+  dimnames(data)[[nameCol]] = data[,nameCol];
+  # Remove rowname column from the data
+  data[nameCol] = NULL;
+  return(data);
+}
+
+# Converts a correaltion matrix to a dataframe while removing redundant data
+correlationToDataFrame = function(correlationData) {
+  # Reference http://stackoverflow.com/questions/7074246/show-correlations-as-an-ordered-list-not-as-a-large-matrix
+  # Flag redundant as NA
+  correlationData[lower.tri(correlationData,diag=TRUE)] = NA;
+  # Convert correlation table to a dataframe
+  correlationData = as.data.frame(as.table(correlationData));
+  # Remove all NA values
+  correlationData = na.omit(correlationData);
+  # Reset the number of the rows
+  rownames(correlationData) = NULL;
+  return(correlationData);
+}
+
+
+# Main funciton to answer all of the question posed in the R_hw1-1.pdf file
 main = function() {
+  # data directories
   geneExpressionFile = "gene_expression_n438x978.txt";
   adrHlgtFile = "ADRs_HLGT_n438c232.txt";
 
-  # Format geneData
-  geneData = read.delim(geneExpressionFile);
-  dimnames(geneData)[[1]] = geneData[,1];
-  geneData = geneData[,-1];
+  drugGeneExpressionData = readTableWithRowNames(geneExpressionFile);
+  drugGeneExpressionData = t(drugGeneExpressionData);
+  drugCorrelation = correlationToDataFrame(cor(drugGeneExpressionData));
 
-  # Reference http://stackoverflow.com/questions/7074246/show-correlations-as-an-ordered-list-not-as-a-large-matrix
-  # Get the correlation of the genes
-  geneCorrelation = cor(geneData);
-  # Mark the values in the bottom half and the diagnal of the correlation as NA because those values are either redundant or meaningless.
-  geneCorrelation[lower.tri(geneCorrelation,diag=TRUE)]=NA;
-  # Convert the matrix to a dataframe
-  geneCorrelation=as.data.frame(as.table(geneCorrelation));
-  # Remove any rows that have a frequency of NA
-  geneCorrelation=na.omit(geneCorrelation);
-  # Get the top most positively correlated genes
-  topPositivelyCorrelatedGenes=geneCorrelation[order(geneCorrelation$Freq, decreasing=TRUE),][1:10,]
-  rownames(topPositivelyCorrelatedGenes) = NULL;
-  topNegativelyCorrelatedGenes=geneCorrelation[order(geneCorrelation$Freq, decreasing=FALSE),][1:10,]
-  rownames(topNegativelyCorrelatedGenes) = NULL;
-  print(topPositivelyCorrelatedGenes);
-  print(topNegativelyCorrelatedGenes);
+  medianDrugCorrelation = median(drugCorrelation[,3]);
+  print(paste("Median Drug Correlation:", medianDrugCorrelation));
+
+  # Get the top most positively correlated drugs
+  topPositivelyCorrelatedDrugs = drugCorrelation[order(drugCorrelation$Freq, decreasing=TRUE),][1:10,]
+  rownames(topPositivelyCorrelatedDrugs) = NULL;
+
+  topPositivelyCorelatedDrug1 = topPositivelyCorrelatedDrugs[1,1];
+  topPositivelyCorelatedDrug2 = topPositivelyCorrelatedDrugs[1,2];
+  print(paste("Top Positively Corelated Drug Pair: (", topPositivelyCorelatedDrug1, ",", topPositivelyCorelatedDrug2, ")"));
+
+  print("Top 10 Most Positively Correlated Drugs")
+  print(topPositivelyCorrelatedDrugs)
+
+  png("topPositivelyCorrelatedDrugs.png")
+  plot(
+    drugGeneExpressionData[,topPositivelyCorelatedDrug1],
+    drugGeneExpressionData[,topPositivelyCorelatedDrug2],
+    xlab=topPositivelyCorelatedDrug1,
+    ylab=topPositivelyCorelatedDrug2,
+    main=paste(topPositivelyCorelatedDrug1, " over ", topPositivelyCorelatedDrug2)
+  )
+
+  # Get the top most negatively correlated drugs
+  topNegativelyCorrelatedDrugs = drugCorrelation[order(drugCorrelation$Freq, decreasing=FALSE),][1:10,]
+  rownames(topNegativelyCorrelatedDrugs) = NULL;
+
+  topNegativelyCorelatedDrug1 = topNegativelyCorrelatedDrugs[1,1];
+  topNegativelyCorelatedDrug2 = topNegativelyCorrelatedDrugs[1,2];
+
+  print(paste("Top Negatively Corelated Drug Pair: (", topNegativelyCorelatedDrug1, ",", topNegativelyCorelatedDrug2, ")"));
+
+  png("topNegativelyCorrelatedDrugs.png")
+  plot(
+    drugGeneExpressionData[, topNegativelyCorelatedDrug1],
+    drugGeneExpressionData[, topNegativelyCorelatedDrug2],
+    xlab=topNegativelyCorelatedDrug1,
+    ylab=topNegativelyCorelatedDrug2,
+    main=paste(topNegativelyCorelatedDrug1, " over ", topNegativelyCorelatedDrug2)
+  )
+
+  print("Top 10 Most Negatively Correlated Drugs")
+  print(topNegativelyCorrelatedDrugs)
 }
 
 main();
