9ccf03151db99fe944f08342cc4d851758f3c45d
Jackkell <bronzeeagle1@hotmail.com>
2017-05-09 19:55:28 -0700

    Worked one question 1 and added a notebook file

--- hw2/hw2.R
+++ hw2/hw2.R
@@ -11,27 +11,67 @@ readTableWithRowNames = function(filename, nameCol = 1) {
   return(data);
 }
 
-main = function() {
-  data = readTableWithRowNames("gene_expression_n438x978.txt")
+questionOne = function() {
+  # Read in the data
+  data = t(readTableWithRowNames("gene_expression_n438x978.txt"))
+  print(dim(data))
   print(data[1:4, 1:3])
 
-  print(dim(data))
-  kmeansResults = kmeans(data, 170)
-  clusters = table(kmeansResults$cluster)
-  print(clusters)
-
-  output = NULL
-
-  groupsSeq = seq(150, 220, 5)
-  for (k in groupsSeq) {
-    print(k)
-    curr.kmeans <- kmeans(data, k)
-    curr.sil <- silhouette(curr.kmeans$cluster, dist(data, method="euclidean"))
-    output <- c(output, mean(curr.sil[, "sil_width"]))
+  roughOutput = NULL
+  # Test all of the possible value of k from 10 to 435 incrementing by 10 to find a rough range of where the best cluster size is
+  stepSize = 20
+  roughClusterSizes = seq(stepSize, nrow(data), stepSize)
+  # The number of trails done to get the averagered average silhouette value for the current groups size (k)
+  trails = 3.0
+  for (k in roughClusterSizes) {
+    averageSilhouetteOnAverage = 0
+    for (i in 1:trails) {
+      print(paste(k, i))
+      currentKmeansResults = kmeans(data, k)
+      currentSilhouetteValues = silhouette(currentKmeansResults$cluster, dist(data, method="euclidean"))
+      averageSilhouetteValue = mean(currentSilhouetteValues[, "sil_width"])
+      averageSilhouetteOnAverage = averageSilhouetteOnAverage + averageSilhouetteValue
+    }
+    averageSilhouetteOnAverage = averageSilhouetteOnAverage / trails
+    roughOutput = c(roughOutput, averageSilhouetteOnAverage)
   }
 
   png ("kmeans_sil.png")
-  plot (groupsSeq, output, type="l", main="kmeans", xlab="number of clusters", ylab="average silhouette")
+  plot (roughClusterSizes, roughOutput, type="l", main="The Average Silhouetee Per Cluster for a\nGiven Number of Clusters on Average", xlab="Number of Clusters", ylab="Average Silhouette Value Per Cluster On Average")
+  names(roughOutput) = roughClusterSizes
+  print(roughOutput)
+
+  # From the results in the output we find that the max is some where around
+  roughBestClusterSize = names(output)[which(output==max(output))]
+  print(roughBestClusterSize)
+  fineOutput = NULL
+  fineClusterSizes = seq(roughBestClusterSize - stepSize, roughBestClusterSize + stepSize)
+  for (k in fineClusterSizes) {
+    averageSilhouetteOnAverage = 0
+    for (i in 1:trails) {
+      print(paste(k, i))
+      currentKmeansResults = kmeans(data, k)
+      currentSilhouetteValues = silhouette(currentKmeansResults$cluster, dist(data, method="euclidean"))
+      averageSilhouetteValue = mean(currentSilhouetteValues[, "sil_width"])
+      averageSilhouetteOnAverage = averageSilhouetteOnAverage + averageSilhouetteValue
+    }
+    averageSilhouetteOnAverage = averageSilhouetteOnAverage / trails
+    fineOutput = c(fineOutput, averageSilhouetteOnAverage)
+  }
+
+
+
+  kmeansResults = kmeans(data, 170)
+  clusters = table(kmeansResults$cluster)
+  print(clusters)
+  print(sum(clusters))
+
+
+}
+
+main = function() {
+  set.seed(12345)
+  questionOne()
 }
 
 main()
