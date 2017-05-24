dbe091f30f8cfa52cfa49a9224f8cda24ff8e666
Jackkell <bronzeeagle1@hotmail.com>
2017-05-24 00:00:55 -0700

    Added another method

--- hw3/hw3.R
+++ hw3/hw3.R
@@ -1,5 +1,6 @@
 library(class)
 library (e1071)
+library(rpart)
 
 # Read the data from a table and converts a column to the row names
 readTableWithRowNames = function(filename, nameCol = 1) {
@@ -45,13 +46,10 @@ noneKNN = function(xData, yData) {
       predictions = c(predictions, prediction)
 
     }
-    print(predictions)
-    print(as.numeric(unlist(yData)))
     accuracy = mean(predictions == unlist(yData))
     accuracies = c(accuracies, accuracy)
   }
   averageAccuracy = mean(accuracies)
-  print(averageAccuracy)
   return(averageAccuracy)
 }
 
@@ -67,7 +65,7 @@ correlation11KNN = function(xDataRaw, yDataRaw) {
   for (r in 1:repeats) {
     # Feature Selection based on correaltion
     correlationVector = abs(apply(xDataRaw, 2, function(x) {cor(unlist(x), unlist(yDataRaw))}))
-    correlatedColumns = names(correlationVector[correlationVector >= quantile(correlationVector, prob=1-50/100)])
+    correlatedColumns = names(correlationVector[correlationVector >= quantile(correlationVector, prob=1-25/100)])
 
     # Shuffle Data
     xData = xDataRaw[sample(nrow(xDataRaw)), correlatedColumns, drop=FALSE];
@@ -88,13 +86,10 @@ correlation11KNN = function(xDataRaw, yDataRaw) {
 
       predictions = c(predictions, results)
     }
-    print(predictions)
-    print(as.numeric(unlist(yData)))
     accuracy = mean(predictions == unlist(yData))
     accuracies = c(accuracies, accuracy)
   }
   averageAccuracy = mean(accuracies)
-  print(averageAccuracy)
   return(averageAccuracy)
 }
 
@@ -115,7 +110,6 @@ noneSvm = function(xDataRaw, yDataRaw) {
 
     predictions = c()
 
-    # 11 - KNN
     for (k in 1:kFolds) {
       testIndexes = which(folds == k, arr.ind=TRUE)
       xTestData = xData[testIndexes, , drop=FALSE]
@@ -127,13 +121,126 @@ noneSvm = function(xDataRaw, yDataRaw) {
       prediction = round(as.numeric(predict(model, xTestData)))
       predictions = c(predictions, prediction)
     }
-    print(predictions)
-    print(as.numeric(unlist(yData)))
-    # print(predictions)
-    # print(yData)
+
     accuracy = mean(predictions == unlist(yData))
     accuracies = c(accuracies, accuracy)
-    # print(accuracy)
+  }
+  averageAccuracy = mean(accuracies)
+  return(averageAccuracy)
+}
+
+correlatedSvm = function(xDataRaw, yDataRaw) {
+  kFolds = 10
+  repeats = 5
+
+  folds = cut(seq(1, nrow(xDataRaw)), breaks=kFolds, labels=FALSE);
+  yDataRaw = yDataRaw[rownames(xDataRaw), , drop=FALSE]
+
+  accuracies = c()
+
+  for (r in 1:repeats) {
+    # Feature Selection based on correaltion
+    correlationVector = abs(apply(xDataRaw, 2, function(x) {cor(unlist(x), unlist(yDataRaw))}))
+    correlatedColumns = names(correlationVector[correlationVector >= quantile(correlationVector, prob=1-25/100)])
+
+    # Shuffle Data
+    xData = xDataRaw[sample(nrow(xDataRaw)), correlatedColumns, drop=FALSE];
+    # print(xData)
+    yData = yDataRaw[rownames(xData), , drop=FALSE];
+
+    predictions = c()
+
+    for (k in 1:kFolds) {
+      testIndexes = which(folds == k, arr.ind=TRUE)
+      xTestData = xData[testIndexes, , drop=FALSE]
+      xTrainData = xData[-testIndexes, , drop=FALSE]
+      yTrainData = yData[-testIndexes, , drop=FALSE]
+      yTestData = yData[testIndexes, , drop=FALSE]
+
+      model = svm(xTrainData, yTrainData)
+      prediction = round(as.numeric(predict(model, xTestData)))
+      predictions = c(predictions, prediction)
+    }
+
+    accuracy = mean(predictions == unlist(yData))
+    accuracies = c(accuracies, accuracy)
+  }
+  averageAccuracy = mean(accuracies)
+  return(averageAccuracy)
+}
+
+noneRPart = function(xDataRaw, yDataRaw) {
+  kFolds = 10
+  repeats = 5
+
+  folds = cut(seq(1, nrow(xDataRaw)), breaks=kFolds, labels=FALSE);
+  yDataRaw = yDataRaw[rownames(xDataRaw), , drop=FALSE]
+
+  accuracies = c()
+
+  for (r in 1:repeats) {
+    # Shuffle Data
+    xData = xDataRaw[sample(nrow(xDataRaw)), , drop=FALSE];
+    # print(xData)
+    yData = yDataRaw[rownames(xData), , drop=FALSE];
+
+    predictions = c()
+
+    for (k in 1:kFolds) {
+      testIndexes = which(folds == k, arr.ind=TRUE)
+      xTestData = xData[testIndexes, , drop=FALSE]
+      xTrainData = xData[-testIndexes, , drop=FALSE]
+      yTrainData = yData[-testIndexes, , drop=FALSE]
+      yTestData = yData[testIndexes, , drop=FALSE]
+
+      model = rpart(as.numeric(unlist(yTrainData))~., data=xTrainData, method="class")
+      prediction = round(as.numeric(predict(model, xTestData)))
+      predictions = c(predictions, prediction)
+    }
+
+    accuracy = mean(predictions == unlist(yData))
+    accuracies = c(accuracies, accuracy)
+  }
+  averageAccuracy = mean(accuracies)
+  print(averageAccuracy)
+  return(averageAccuracy)
+}
+
+correlatedRPart = function(xDataRaw, yDataRaw) {
+  kFolds = 10
+  repeats = 5
+
+  folds = cut(seq(1, nrow(xDataRaw)), breaks=kFolds, labels=FALSE);
+  yDataRaw = yDataRaw[rownames(xDataRaw), , drop=FALSE]
+
+  accuracies = c()
+
+  for (r in 1:repeats) {
+    # Feature Selection based on correaltion
+    correlationVector = abs(apply(xDataRaw, 2, function(x) {cor(unlist(x), unlist(yDataRaw))}))
+    correlatedColumns = names(correlationVector[correlationVector >= quantile(correlationVector, prob=1-25/100)])
+
+    # Shuffle Data
+    xData = xDataRaw[sample(nrow(xDataRaw)), correlatedColumns, drop=FALSE];
+    # print(xData)
+    yData = yDataRaw[rownames(xData), , drop=FALSE];
+
+    predictions = c()
+
+    for (k in 1:kFolds) {
+      testIndexes = which(folds == k, arr.ind=TRUE)
+      xTestData = xData[testIndexes, , drop=FALSE]
+      xTrainData = xData[-testIndexes, , drop=FALSE]
+      yTrainData = yData[-testIndexes, , drop=FALSE]
+      yTestData = yData[testIndexes, , drop=FALSE]
+
+      model = rpart(as.numeric(unlist(yTrainData))~., data=xTrainData, method="class")
+      prediction = round(as.numeric(predict(model, xTestData)))
+      predictions = c(predictions, prediction)
+    }
+
+    accuracy = mean(predictions == unlist(yData))
+    accuracies = c(accuracies, accuracy)
   }
   averageAccuracy = mean(accuracies)
   print(averageAccuracy)
@@ -150,8 +257,15 @@ main = function() {
   # print(adrData[1:5, 1:3])
   # print(geneData[1:5, 1:3])
 
-  for (i in 1:1) {
-    noneKNN(geneData, adrData[i])
+  methodNames = c("noneKNN", "correlation11KNN", "noneSvm", "correlatedSvm")
+  methods = c(noneKNN, correlation11KNN, noneSvm, correlatedSvm)
+
+  for (i in 1:3) {
+    print(i)
+    noneRPart(geneData, adrData[i])
+    # for (method in methods) {
+    #   print(method(geneData, adrData[i]))
+    # }
   }
   return("Finished Without Problems")
 }
